98f1e994ec9f41b777dd965bd7d5d1d80c2c9a96
raffitz <raf.a.m.c.gon@gmail.com>
2016-04-16 11:46:15 +0100

    Convert spinning poly test into intro screen

--- game.py
+++ game.py
@@ -5,6 +5,33 @@ import pygame.gfxdraw
 import pygame.draw
 import math
 
+# Color declaration:
+
+colors = {	'black':	(0,0,0),
+		'white':	(255,255,255),
+		'red':		(255,0,0),
+		'green':	(0,255,0),
+		'blue':		(0,0,255),
+		'yellow':	(255,255,0),
+		'cyan':		(0,255,255),
+		'magenta':	(255,0,255),
+		'azure':	(0,127,255),
+		'violet':	(127,0,255),
+		'rose':		(255,0,127),
+		'orange':	(255,127,0),
+		'chartreuse':	(127,255,0),
+		'spring':	(0,255,127)}
+
+primaries = ['red','green','blue']
+secondaries = ['yellow','cyan','magenta']
+tertiaries = ['azure','violet','rose','orange','chartreuse','spring']
+
+saturated = primaries + secondaries + tertiaries
+
+bw = ['black','white']
+
+allcols = bw + saturated
+
 # Draw an ngon:
 def ngon(targetsurface,centerx,centery,n,radius,angle,incolor,bordercolor,border):
 	if n < 3:
@@ -16,7 +43,8 @@ def ngon(targetsurface,centerx,centery,n,radius,angle,incolor,bordercolor,border
 		vertcomp = radius * math.sin(finangle)
 		polyverts.append( (centerx + horizcomp,centery + vertcomp) )
 	pygame.draw.polygon(targetsurface,incolor,polyverts,0)
-	pygame.draw.polygon(targetsurface,bordercolor,polyverts,border)
+	if border != 0:
+		pygame.draw.polygon(targetsurface,bordercolor,polyverts,border)
 
 
 
@@ -48,6 +76,15 @@ running = True
 
 # Game tick counter:
 tick = 0
+loops = 0
+
+# Game state:
+state = 0
+#	States:
+#	0	Intro
+#	1	Game
+#	2	Pause
+#	3	Outro
 
 # Main loop:
 while running:
@@ -70,6 +107,25 @@ while running:
 					gameDisp = pygame.display.set_mode(modes[0],pygame.FULLSCREEN)
 				disps = pygame.display.get_surface()
 				pxwidth,pxheight = disps.get_size()
+			#else:
+				#if state == 0:
+					# Intro
+				#elif state == 1:
+					# Game
+				#elif state == 2:
+					# Pause
+				#else:
+					# Outro
+		elif each_event.type == pygame.KEYUP:
+			if state == 0:
+				# Intro
+				state = 1
+			#elif state == 1:
+				#Game
+			#elif state == 2:
+				#Pause
+			#else:
+				#Outro
 		elif each_event.type == pygame.QUIT:
 			# If you wanna exit the window, we'll let'ya
 			running = False
@@ -83,15 +139,28 @@ while running:
 	
 	# Clear screen:
 	gamefield.fill((255,255,255))
-
-	# Test polygon draw:
-	ngon(gamefield,320,180,3,10,(tick*math.pi*2)/30,(255,0,0),(0,0,0),1)
+	
+	# States:
+	if state == 0:
+		# Intro
+		# Test polygon draw:
+		ngon(gamefield,180,180,3 + ((loops//12) % 15),50,(tick*math.pi*2)/30,colors[saturated[loops % 12]],(0,0,0),1)
+	#elif state == 1:
+		#Game
+	#elif state == 2:
+		#Pause
+	#else:
+		#Outro
+	
 
 
 	# Framerate limiting
 	clock.tick(30)
 
-	tick = (tick + 1) % 30
+	tick = tick + 1
+	if tick > 30:
+		tick = tick - 30
+		loops += 1
 	
 	# Placing the game field on screen
 	scale = min(int(pxwidth*1.0/(1.0*gamewidth)),int(pxheight*1.0/(1.0*gameheight)))
